636e9414f48d072a20bed2549dc1ed2ca812210c
Jeremy Frank <jfrank@summitinvest.tech>
2024-12-08 15:21:49 -0700

    add webSearchTool to tools

--- auto-kol/agent/src/tools/index.ts
+++ auto-kol/agent/src/tools/index.ts
@@ -3,6 +3,7 @@ import { createTweetSearchTool } from './tools/tweetSearchTool.js';
 import { createQueueResponseTool } from './tools/queueResponseTool.js';
 import { createQueueSkippedTool } from './tools/queueSkippedTool.js';
 import { createSearchSimilarTweetsTool } from './tools/searchSimilarTweetsTool.js';
+import { createWebSearchTool } from './tools/webSearchTool.js';
 
 export const createTools = (scraper: any) => {
 
@@ -16,12 +17,15 @@ export const createTools = (scraper: any) => {
 
     const searchSimilarTweetsTool = createSearchSimilarTweetsTool();
 
+    const webSearchTool = createWebSearchTool();
+
     return {
         tweetSearchTool,
         queueResponseTool,
         queueSkippedTool,
         searchSimilarTweetsTool,
         fetchTimelineTool,
-        tools: [tweetSearchTool, queueResponseTool, queueSkippedTool, searchSimilarTweetsTool, fetchTimelineTool]
+        webSearchTool,
+        tools: [tweetSearchTool, queueResponseTool, queueSkippedTool, searchSimilarTweetsTool, fetchTimelineTool, webSearchTool]
     };
 };
