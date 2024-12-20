39e608314b8a1d59ec98dc59a4cc326867954556
Marc-Aurele Besner <82244926+marc-aurele-besner@users.noreply.github.com>
2024-12-20 07:14:40 -0500

    add ga analytics to agent-memory-viewer

--- auto-kol/agent-memory-viewer/src/App.tsx
+++ auto-kol/agent-memory-viewer/src/App.tsx
@@ -3,6 +3,7 @@ import { Routes, Route } from 'react-router-dom'
 import Navbar from './components/Navbar'
 import MemoryViewer from './pages/MemoryViewer'
 import DSNViewer from './components/DSNViewer'
+import Analytics from './components/Analytics'
 
 function App() {
     return (
@@ -10,6 +11,7 @@ function App() {
             <Navbar />
             <Box as="main" p={4}>
                 <Routes>
+                <Analytics />
                     <Route path="/" element={<DSNViewer />} />
                     <Route path="/memory/:cid" element={<MemoryViewer />} />
                 </Routes>
