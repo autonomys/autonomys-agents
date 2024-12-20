80bef76cc79fb5431fa9afed8264d1962544ab8e
Marc-Aurele Besner <82244926+marc-aurele-besner@users.noreply.github.com>
2024-12-20 16:30:46 -0500

    add verify script

---
+++ auto-agent-contracts/script/verify.sh
@@ -0,0 +1,3 @@
+# Change the contract address and name if needed
+
+forge verify-contract --verifier blockscout --verifier-url https://blockscout.taurus.autonomys.xyz/api/ 0xc1afebe677badb71fc760e61479227e43b422e48 AgentMemory
