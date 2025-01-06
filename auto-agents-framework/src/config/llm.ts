ecad72ced0036fcef0dc1d261123889248dfb029
xm0onh <saeedyazdinezhad@gmail.com>
2025-01-05 17:29:58 -0800

    prettier

--- auto-agents-framework/src/config/llm.ts
+++ auto-agents-framework/src/config/llm.ts
@@ -1,9 +1,8 @@
-
 export const llmConfig = {
-    DECISION_LLM_PROVIDER: 'anthropic',
-    ANALYZE_LLM_PROVIDER: 'anthropic',
-    GENERATION_LLM_PROVIDER: 'anthropic',
-    RESPONSE_LLM_PROVIDER: 'anthropic',
-    LARGE_LLM_MODEL: 'claude-3-opus-20240229',
-    SMALL_LLM_MODEL: 'claude-3-5-sonnet-20240620',
-}
+  DECISION_LLM_PROVIDER: 'anthropic',
+  ANALYZE_LLM_PROVIDER: 'anthropic',
+  GENERATION_LLM_PROVIDER: 'anthropic',
+  RESPONSE_LLM_PROVIDER: 'anthropic',
+  LARGE_LLM_MODEL: 'claude-3-opus-20240229',
+  SMALL_LLM_MODEL: 'claude-3-5-sonnet-20240620',
+};
