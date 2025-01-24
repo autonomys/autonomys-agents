9dee1ae308f1273a487bf898b921e02e2a3e2433
xm0onh <saeedyazdinezhad@gmail.com>
2025-01-23 18:28:43 -0800

    Add rest of the twitter tools

--- src/agents/workflows/orchestrator/tools.ts
+++ src/agents/workflows/orchestrator/tools.ts
@@ -2,6 +2,8 @@ import { createTwitterWorkflowTool } from './workflowTools/twitterWorkflowTool.j
 import { createFetchMentionsTool } from '../../tools/twitter/fetchMentionsTool.js';
 import { createFetchTimelineTool } from '../../tools/twitter/fetchTimelineTool.js';
 import { createPostTweetTool } from '../../tools/twitter/postTweetTool.js';
+import { createFetchMyRecentRepliesTool } from '../../tools/twitter/fetchMyRecentRepliesTool.js';
+import { createFetchMyRecentTweetsTool } from '../../tools/twitter/fetchMyRecentTweetsTool.js';
 import { TwitterApi } from '../../../services/twitter/types.js';
 
 export const createTools = (twitterApi: TwitterApi) => {
@@ -9,12 +11,23 @@ export const createTools = (twitterApi: TwitterApi) => {
   const fetchMentionsTool = createFetchMentionsTool(twitterApi);
   const fetchTimelineTool = createFetchTimelineTool(twitterApi);
   const postTweetTool = createPostTweetTool(twitterApi);
+  const fetchMyRecentRepliesTool = createFetchMyRecentRepliesTool(twitterApi);
+  const fetchMyRecentTweetsTool = createFetchMyRecentTweetsTool(twitterApi);
 
   return {
     twitterWorkflowTool,
     fetchMentionsTool,
     fetchTimelineTool,
     postTweetTool,
-    tools: [twitterWorkflowTool, fetchMentionsTool, fetchTimelineTool, postTweetTool],
+    fetchMyRecentRepliesTool,
+    fetchMyRecentTweetsTool,
+    tools: [
+      twitterWorkflowTool,
+      fetchMentionsTool,
+      fetchTimelineTool,
+      postTweetTool,
+      fetchMyRecentRepliesTool,
+      fetchMyRecentTweetsTool,
+    ],
   };
 };
