83d43185aa3c693202250b83e5924a8db0171d0b
xm0onh <saeedyazdinezhad@gmail.com>
2025-01-24 21:21:45 -0800

    Add footer

--- agent-memory-viewer/frontend/src/App.tsx
+++ agent-memory-viewer/frontend/src/App.tsx
@@ -1,22 +1,24 @@
-import { Box } from '@chakra-ui/react'
+import { Box, Flex } from '@chakra-ui/react'
 import { Routes, Route } from 'react-router-dom'
 import Navbar from './components/Navbar'
+import { Footer } from './components/Footer'
 import MemoryViewer from './pages/MemoryViewer'
 import DSNViewer from './pages/DSNViewer'
 import Analytics from './components/Analytics'
 
 function App() {
     return (
-        <Box minH="100vh">
+        <Flex direction="column" minH="100vh">
             <Navbar />
             <Analytics />
-            <Box as="main" p={4}>
+            <Box as="main" p={4} flex="1">
                 <Routes>
                     <Route path="/" element={<DSNViewer />} />
                     <Route path="/memory/:cid" element={<MemoryViewer />} />
                 </Routes>
             </Box>
-        </Box>
+            <Footer />
+        </Flex>
     )
 }
 
