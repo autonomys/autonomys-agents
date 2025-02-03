76a8b6363df0594802f56a32f49856ee363381ec
xm0onh <saeedyazdinezhad@gmail.com>
2025-02-03 14:07:13 -0800

    clean coding the llm cfg - remove models type

--- src/config/llm.ts
+++ src/config/llm.ts
@@ -1,35 +1,35 @@
-import { llmModels, LLMNodeConfiguration, LLMProvider } from '../services/llm/types.js';
+import { LLMNodeConfiguration, LLMProvider } from '../services/llm/types.js';
 
 export const llmDefaultConfig = {
   nodes: {
     decision: {
       provider: LLMProvider.OPENAI,
-      model: llmModels.openai.gpt4o_mini,
+      model: 'gpt-4o-mini',
       temperature: 0.2,
     } as LLMNodeConfiguration,
     analyze: {
       provider: LLMProvider.ANTHROPIC,
-      model: llmModels.anthropic.claude35sonnet,
+      model: 'claude-3-5-sonnet-latest',
       temperature: 0.5,
     } as LLMNodeConfiguration,
     generation: {
       provider: LLMProvider.ANTHROPIC,
-      model: llmModels.anthropic.claude35sonnet,
+      model: 'claude-3-5-sonnet-latest',
       temperature: 0.8,
     } as LLMNodeConfiguration,
     response: {
       provider: LLMProvider.OPENAI,
-      model: llmModels.openai.gpt4o_mini,
+      model: 'gpt-4o-mini',
       temperature: 0.8,
     } as LLMNodeConfiguration,
     orchestrator: {
       provider: LLMProvider.ANTHROPIC,
-      model: llmModels.anthropic.claude35sonnet,
+      model: 'claude-3-5-sonnet-latest',
       temperature: 0.2,
     } as LLMNodeConfiguration,
     prompt_summarizer: {
       provider: LLMProvider.OPENAI,
-      model: llmModels.openai.gpt4o_mini,
+      model: 'gpt-4o-mini',
       temperature: 0.2,
     } as LLMNodeConfiguration,
   },
--- src/services/llm/types.ts
+++ src/services/llm/types.ts
@@ -15,22 +15,3 @@ export type LLMNodeConfiguration = {
   model: string;
   temperature: number;
 };
-
-export const llmModels = {
-  openai: {
-    gpt4_turbo: 'gpt-4-turbo-preview',
-    gpt4o_mini: 'gpt-4o-mini',
-    o3_mini: 'o3-mini',
-  },
-  anthropic: {
-    claude35sonnet: 'claude-3-5-sonnet-latest',
-    claude35haiku: 'claude-3-5-haiku-latest',
-  },
-  deepseek: {
-    deepseekChat: 'deepseek-chat',
-    deepseekReasoner: 'deepseek-reasoner',
-  },
-  ollama: {
-    llama3: 'llama3.1',
-  },
-};
