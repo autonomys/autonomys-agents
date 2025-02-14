41e4c27e8976788e92b111c3585c2f5c14909133
Marc-Aurele Besner <82244926+marc-aurele-besner@users.noreply.github.com>
2025-02-14 08:21:35 -0500

    clean-up and add more faucet tools

--- src/agents/tools/faucetTools.ts
+++ src/agents/tools/faucetTools.ts
@@ -1,15 +1,77 @@
-import { AIMessage } from '@langchain/core/messages';
 import { DynamicStructuredTool } from '@langchain/core/tools';
-import { ToolNode } from '@langchain/langgraph/prebuilt';
 import { z } from 'zod';
-import { createLogger } from '../../utils/logger.js';
-import { requestTokens } from './utils/faucet.js';
+import {
+  isMinter,
+  lockTime,
+  nextAccessTime,
+  requestTokens,
+  withdrawalAmount,
+} from './utils/faucet.js';
 
-const logger = createLogger('send-faucet-request-tool');
-
-export const createSendFaucetRequestTool = () =>
+export const createFaucetLockTimeTool = () =>
   new DynamicStructuredTool({
-    name: 'send_faucet_request',
+    name: 'faucet_lock_time',
+    description: `
+    Get the lock time for the faucet contract on the Auto EVM Autonomy Network.
+    USE THIS WHEN:
+    - You need to know the standard lock time between requests.
+    `,
+    schema: z.object({}),
+    func: async () => {
+      const result = await lockTime();
+      return result;
+    },
+  });
+
+export const createFaucetNextAccessTimeTool = () =>
+  new DynamicStructuredTool({
+    name: 'faucet_next_access_time',
+    description: `
+    Get the next access time for the faucet contract on the Auto EVM Autonomy Network for a specific wallet.
+    USE THIS WHEN:
+    - You need to know when the faucet will be unlocked for a specific wallet.
+    `,
+    schema: z.object({ address: z.string() }),
+    func: async ({ address }) => {
+      const result = await nextAccessTime(address);
+      return result;
+    },
+  });
+
+export const createFaucetIsMinterTool = () =>
+  new DynamicStructuredTool({
+    name: 'faucet_is_minter',
+    description: `
+    Get the isMinter status for the faucet contract on the Auto EVM Autonomy Network for a specific wallet.
+    USE THIS WHEN:
+    - You need to know if the wallet is a minter for the faucet contract on the Auto EVM Autonomy Network.
+    This allow you to verify if the wallet you are using to send the request is actually allowed to do so.
+    `,
+    schema: z.object({ address: z.string() }),
+    func: async ({ address }) => {
+      const result = await isMinter(address);
+      return result;
+    },
+  });
+
+export const createFaucetWithdrawalAmountTool = () =>
+  new DynamicStructuredTool({
+    name: 'faucet_withdrawal_amount',
+    description: `
+    Get the withdrawal amount for the faucet contract on the Auto EVM Autonomy Network.
+    USE THIS WHEN:
+    - You need to know the amount of tokens the faucet is set to dispatch.
+    `,
+    schema: z.object({}),
+    func: async () => {
+      const result = await withdrawalAmount();
+      return result;
+    },
+  });
+
+export const createFaucetRequestTokensTool = () =>
+  new DynamicStructuredTool({
+    name: 'faucet_request_tokens',
     description: `
     Send a token request to the faucet contract on the Auto EVM Autonomy Network.
     USE THIS WHEN:
@@ -18,34 +80,23 @@ export const createSendFaucetRequestTool = () =>
     `,
     schema: z.object({ address: z.string() }),
     func: async ({ address }) => {
-      try {
-        logger.info('Sending faucet request - Received data:', JSON.stringify(address, null, 2));
-        const result = await requestTokens(address);
-        logger.info('Sending faucet request - Transaction:', JSON.stringify(result, null, 2));
-        return result;
-      } catch (error) {
-        logger.error('Error sending faucet request:', error);
-        throw error;
-      }
+      const result = await requestTokens(address);
+      return result;
     },
   });
 
-export const invokeSendFaucetRequestTool = async (toolNode: ToolNode, address: string) => {
-  logger.info('Invoking send faucet request tool with data:', JSON.stringify(address, null, 2));
-  const toolResponse = await toolNode.invoke({
-    messages: [
-      new AIMessage({
-        content: '',
-        tool_calls: [
-          {
-            name: 'send_faucet_request',
-            args: { address },
-            id: 'send_faucet_request_call',
-            type: 'tool_call',
-          },
-        ],
-      }),
-    ],
-  });
-  return toolResponse;
+export const createAllFaucetTools = () => {
+  const faucetLockTimeTool = createFaucetLockTimeTool();
+  const faucetNextAccessTimeTool = createFaucetNextAccessTimeTool();
+  const faucetIsMinterTool = createFaucetIsMinterTool();
+  const faucetWithdrawalAmountTool = createFaucetWithdrawalAmountTool();
+  const faucetRequestTokensTool = createFaucetRequestTokensTool();
+
+  return [
+    faucetLockTimeTool,
+    faucetNextAccessTimeTool,
+    faucetIsMinterTool,
+    faucetWithdrawalAmountTool,
+    faucetRequestTokensTool,
+  ];
 };
--- src/agents/tools/utils/faucet.ts
+++ src/agents/tools/utils/faucet.ts
@@ -1,34 +1,90 @@
 import { Contract } from 'ethers';
-import { config } from '../../../config/index.js';
 import { createLogger } from '../../../utils/logger.js';
 import { FAUCET_ABI } from './abi/faucet.js';
 import { wallet } from './blockchain/agentWallet.js';
 
-const logger = createLogger('faucet-tool');
+const logger = createLogger('faucet-tools');
 const FAUCET_CONTRACT_ADDRESS = '0x2296dbb90C714c1355Ff9cbcB70D5AB29060b454';
 
-export async function requestTokens(address: string) {
-  logger.info('Sending faucet request - Starting requestTokens');
+type FaucetLockTimeResult = {
+  lockTime: BigInt;
+};
 
-  if (!config.blockchainConfig.PRIVATE_KEY) {
-    logger.error('Private key is not set');
-    return {
-      success: false,
-      txHash: null,
-      message: 'Private key is not set',
-    };
-  }
+type FaucetNextAccessTimeResult = {
+  nextAccessTime: BigInt;
+};
 
+type FaucetIsMinterResult = {
+  isMinter: boolean;
+};
+
+type FaucetWithdrawalAmountResult = {
+  withdrawalAmount: BigInt;
+};
+
+type FaucetRequestResult = {
+  success: boolean;
+  txHash: string | null;
+  message: string;
+};
+
+const getFaucetContract = () => {
+  return new Contract(FAUCET_CONTRACT_ADDRESS, FAUCET_ABI, wallet);
+};
+
+export const lockTime = async (): Promise<FaucetLockTimeResult> => {
   try {
-    const faucet = new Contract(FAUCET_CONTRACT_ADDRESS, FAUCET_ABI, wallet);
+    const faucet = getFaucetContract();
+    const lockTime = await faucet.lockTime();
+    return { lockTime };
+  } catch (error) {
+    logger.error('Error calling lockTime:', error);
+    throw error;
+  }
+};
 
+export const nextAccessTime = async (address: string): Promise<FaucetNextAccessTimeResult> => {
+  try {
+    const faucet = getFaucetContract();
+    const nextAccessTime = await faucet.nextAccessTime(address);
+    return { nextAccessTime };
+  } catch (error) {
+    logger.error('Error calling nextAccessTime:', error);
+    throw error;
+  }
+};
+
+export const isMinter = async (address: string): Promise<FaucetIsMinterResult> => {
+  try {
+    const faucet = getFaucetContract();
+    const isMinter = await faucet.isMinter(address);
+    return { isMinter };
+  } catch (error) {
+    logger.error('Error calling isMinter:', error);
+    throw error;
+  }
+};
+
+export const withdrawalAmount = async (): Promise<FaucetWithdrawalAmountResult> => {
+  try {
+    const faucet = getFaucetContract();
+    const withdrawalAmount = await faucet.withdrawalAmount();
+    return { withdrawalAmount };
+  } catch (error) {
+    logger.error('Error calling withdrawalAmount:', error);
+    throw error;
+  }
+};
+
+export const requestTokens = async (address: string): Promise<FaucetRequestResult> => {
+  logger.info('Sending faucet request - Starting requestTokens');
+  try {
+    const faucet = getFaucetContract();
     const tx = await faucet.requestTokens(address);
-
     logger.info('Faucet request transaction submitted', {
       success: true,
       txHash: tx.hash,
     });
-
     return {
       success: true,
       txHash: tx.hash,
@@ -42,4 +98,4 @@ export async function requestTokens(address: string) {
       message: 'Error requesting tokens',
     };
   }
-}
+};
