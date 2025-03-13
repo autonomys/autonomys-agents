83b2cd57c263280ab40a715e6d9d78ab201b61fc
xm0onh <saeedyazdinezhad@gmail.com>
2025-03-12 17:01:49 -0700

    integrate character api route with web-cli

---
+++ web-cli/src/services/CharacterService.ts
@@ -0,0 +1,6 @@
+import { apiRequest } from './Api';
+
+export const getCharacterName = async () => {
+    const response = await apiRequest<{ character: string }>('/character/name');
+    return response.character;
+};
