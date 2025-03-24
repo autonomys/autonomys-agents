effd0d640ff2c15d4a335c387ec19c9d62e57719
xm0onh <saeedyazdinezhad@gmail.com>
2025-03-23 17:20:19 -0700

    update network default config to mainnet

--- agent-pkg/src/config/default.ts
+++ agent-pkg/src/config/default.ts
@@ -1,5 +1,5 @@
 export const DEFAULT_CONFIG = {
-  autoDriveNetwork: 'taurus',
+  autoDriveNetwork: 'mainnet',
   taurusRpcUrl: 'https://auto-evm.taurus.autonomys.xyz/ws',
   packageRegistryAddress: '0xE90A8DceD16255Ba6D589808ccb2E50a74C80A8c',
   autoSaveCredentials: true,
