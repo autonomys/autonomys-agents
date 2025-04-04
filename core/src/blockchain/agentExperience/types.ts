c1ee7ee30c6f63d352cfff036dfc191742890b2d
Jeremy Frank <jeremy@subspace.network>
2025-04-04 22:09:15 +0000

    update saveLastMemoryCid to return undefined on blockchain save failure and improve error handling

--- core/src/blockchain/agentExperience/types.ts
+++ core/src/blockchain/agentExperience/types.ts
@@ -58,7 +58,7 @@ export type ExperienceManagerOptions = {
 
 export type CidManager = {
   getLastMemoryCid: () => Promise<string>;
-  saveLastMemoryCid: (cid: string) => Promise<ethers.TransactionReceipt>;
+  saveLastMemoryCid: (cid: string) => Promise<ethers.TransactionReceipt | undefined>;
   localHashStatus: { message: string };
 };
 
