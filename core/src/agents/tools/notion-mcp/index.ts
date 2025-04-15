aadb5770d8893539f82ce84f089bad24765aa64c
Jeremy Frank <jfrank@summitinvest.tech>
2025-04-15 15:49:33 -0600

    add notion-mcp tool

---
+++ core/src/agents/tools/notion-mcp/index.ts
@@ -0,0 +1,16 @@
+import { logger } from '../agentExperiences/index.js';
+import { createMcpClientTool } from '../mcp-tool/index.js';
+import { StdioServerParameters } from '@modelcontextprotocol/sdk/client/stdio.js';
+
+export const createNotionTools = async (integrationSecret: string) => {
+  const notionServerParams: StdioServerParameters = {
+    command: process.execPath,
+    args: ['node_modules/.bin/notion-mcp-server'],
+    env: {
+      OPENAPI_MCP_HEADERS: `{\"Authorization\": \"Bearer ${integrationSecret}\", \"Notion-Version\": \"2022-06-28\" }`,
+    },
+  };
+  const tools = await createMcpClientTool('notion-mcp', '0.0.1', notionServerParams);
+  logger.debug('notionTools', { tools });
+  return tools;
+};
