a8bdf87a071e1dbd137c16579266ca444589f687
xm0onh <saeedyazdinezhad@gmail.com>
2025-04-28 16:10:07 -0700

    update web-cli to accept var injection after built

--- web-cli/src/react-app-env.d.ts
+++ web-cli/src/react-app-env.d.ts
@@ -1,1 +1,10 @@
 /// <reference types="react-scripts" />
+
+declare global {
+  interface Window {
+    runtimeConfig?: {
+      API_BASE_URL?: string;
+      API_TOKEN?: string;
+    };
+  }
+}
--- web-cli/src/services/Api.ts
+++ web-cli/src/services/Api.ts
@@ -1,5 +1,31 @@
-export const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || `https://localhost:3010/api`;
-export const API_TOKEN = process.env.REACT_APP_API_TOKEN || '';
+const runtimeConfig = typeof window !== 'undefined' ? (window as any).runtimeConfig || {} : {};
+
+let determinedApiBaseUrl: string;
+
+if (runtimeConfig.API_BASE_URL && runtimeConfig.API_BASE_URL !== 'your_api_url_here') {
+  determinedApiBaseUrl = runtimeConfig.API_BASE_URL;
+}
+else if (process.env.REACT_APP_API_BASE_URL) {
+  determinedApiBaseUrl = process.env.REACT_APP_API_BASE_URL;
+}
+else {
+  determinedApiBaseUrl = 'https://localhost:3010/api';
+}
+
+let determinedApiToken: string;
+
+if (runtimeConfig.API_TOKEN && runtimeConfig.API_TOKEN !== 'your_api_token_here') {
+  determinedApiToken = runtimeConfig.API_TOKEN;
+}
+else if (process.env.REACT_APP_API_TOKEN) {
+  determinedApiToken = process.env.REACT_APP_API_TOKEN;
+}
+else {
+  determinedApiToken = '';
+}
+
+export const API_BASE_URL = determinedApiBaseUrl;
+export const API_TOKEN = determinedApiToken;
 
 export const DEFAULT_NAMESPACE = 'orchestrator';
 
